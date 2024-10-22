ab7ac6b05ec5dd31b15b7d652d7d8db5ad9e1d24
Uria <dorman010@gmail.com>
2024-10-23 00:07:11 +0100

    pretty print functiopnality, starting tree functionality

--- tree.js
+++ tree.js
@@ -3,28 +3,47 @@ import { mergeSort } from "./mergeSort.js";
 export class Tree {
   constructor(arr) {
     this.buildTree(arr);
-    this.root = null;
+    this.root;
   }
   buildTree(arr) {
-    let n = arr.length;
-    if (n == 0) {
-      return;
+    if (arr.length == 0 || arr.length == 1) {
+      this.root = arr[0];
     }
-    //create root node
     arr = mergeSort(arr);
     arr = [...new Set(arr)];
-    let mid = Math.floor(n / 2);
+    //create root node
+    let mid = Math.floor(arr.length / 2);
     let rootNode = new Node(arr[mid]);
+    arr[mid] = null;
     let start = 0;
-    let end = n - 1;
+    let end = arr.length - 1;
     this.root = rootNode;
     // if left subtree exists
-    if (arr[start] < mid) {
-      this.root.left = arr[start];
+    while (arr[start] !== null) {
+      this.root.left = new Node(arr[start]);
+      start++;
     }
-    if (arr[end] > mid) {
-      this.root.right = arr[end];
-    }
+    console.log(arr[start]);
     // if right subtree exists
+    if (arr[end] > rootNode.data) {
+      this.root.right = new Node(arr[end]);
+    }
+    return this.root;
+  }
+  prettyPrint(node, prefix = "", isLeft = true) {
+    if (node === null) {
+      return;
+    }
+    if (node.right !== null) {
+      this.prettyPrint(
+        node.right,
+        `${prefix}${isLeft ? "│   " : "    "}`,
+        false
+      );
+    }
+    console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+    if (node.left !== null) {
+      this.prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+    }
   }
 }
