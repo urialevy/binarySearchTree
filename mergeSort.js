8a16d0b48be982d2584902e9734a27d307a77c51
Uria <dorman010@gmail.com>
2024-10-22 23:21:28 +0100

    add mergesort to ensure array is sorted at O(n (log n))

---
+++ mergeSort.js
@@ -0,0 +1,21 @@
+function merge(leftArr, rightArr) {
+  const tmp = [];
+  while (leftArr.length > 0 && rightArr.length > 0) {
+    leftArr[0] <= rightArr[0]
+      ? tmp.push(leftArr.shift())
+      : tmp.push(rightArr.shift());
+  }
+  return [...tmp, ...leftArr, ...rightArr];
+}
+
+export function mergeSort(arr) {
+  if (arr.length < 2) {
+    return arr;
+  }
+
+  const midpoint = Math.floor(arr.length / 2);
+  const leftHalf = arr.slice(0, midpoint);
+  const rightHalf = arr.slice(midpoint, arr.length);
+  return merge(mergeSort(leftHalf), mergeSort(rightHalf));
+}
+const smallArr = [21, 13, 8, 5, 3, 2, 1, 1, 0];
