4a1ad734051d5a1ef64f57fbc18bd9439cc70915
Uria <dorman010@gmail.com>
2024-10-23 23:42:35 +0100

    root node func, figuring out the rest...

--- tree.js
+++ tree.js
@@ -9,25 +9,43 @@ export class Tree {
     if (arr.length == 0 || arr.length == 1) {
       this.root = arr[0];
     }
-    arr = mergeSort(arr);
     arr = [...new Set(arr)];
+    arr = mergeSort(arr);
+
     //create root node
     let mid = Math.floor(arr.length / 2);
+    console.log(arr.length / 2);
     let rootNode = new Node(arr[mid]);
+    let currentNode = rootNode;
     arr[mid] = null;
     let start = 0;
     let end = arr.length - 1;
     this.root = rootNode;
     // if left subtree exists
-    while (arr[start] !== null) {
+    if (arr[start] < rootNode.data) {
       this.root.left = new Node(arr[start]);
       start++;
     }
-    console.log(arr[start]);
     // if right subtree exists
     if (arr[end] > rootNode.data) {
-      this.root.right = new Node(arr[end]);
+      this.root.right = new Node(arr[mid + 1]);
     }
+    currentNode = this.root.left;
+    while (arr[start] !== null) {
+      if (arr[start] < currentNode.data) {
+        currentNode.left = new Node(arr[start]);
+        currentNode = currentNode.left;
+      } else {
+        currentNode.right = new Node(arr[start]);
+        currentNode = currentNode.right;
+      }
+      start++;
+    }
+    currentNode = this.root.right;
+    for (let i = mid + 2; i < arr.length; i++) {
+      console.log(arr[i]);
+    }
+    console.log(arr);
     return this.root;
   }
   prettyPrint(node, prefix = "", isLeft = true) {
